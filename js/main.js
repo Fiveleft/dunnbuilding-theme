f3f6b50a16487726b2a72c9a6dbc0ec917d5c9c3
Charles Krueger <ck@fiveleft.com>
2015-09-08 17:16:04 -0500

    Initial Node/Bower installations Setting up initial JS files

---
+++ js/main.js
@@ -0,0 +1,33 @@
+ require.config({
+  paths: {
+    jquery: "vendor/jquery/jquery",
+    backbone: "vendor/backbone/backbone",
+    underscore: "vendor/underscore/underscore",
+    // utils: "fiveleft/core/Utils",
+    // sharrre: "libs/jquery.sharrre",
+    // application: "app/Application",
+    // breakpoints: "app/Breakpoints",
+    // imageload: "app/ImageLoad",
+    // newscrawler: "app/modules/NewsCrawler",
+    // archiveview: "app/views/ArchiveView",
+    // commentsview: "app/views/CommentsView",
+    // homeview: "app/views/HomeView",
+    // pageview: "app/views/PageView",
+    // postview: "app/views/PostView",
+    raf: "app/utils/RAF"
+  },
+  shim: {
+    backbone: {
+      deps: ['jquery', 'underscore'],
+      exports: 'backbone'
+    }
+    raf : {
+      exports: 'raf'
+    }
+  }
+});
+require(
+  ['jquery','application'], 
+  function($, Application) {
+    new Application();
+  });
